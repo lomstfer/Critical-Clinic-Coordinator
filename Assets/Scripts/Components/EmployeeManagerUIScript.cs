9fcfeaa0869263cb7f293bb6e4c861382f46aa28
MaxWesterlund <max.westerlund3@gmail.com>
2023-07-25 18:53:41 +0200

    Revert "Merge branch 'main' of https://github.com/lomstfer/Applicationeer"
    This reverts commit 65a23a36d238744557f4f33cc7df1078184144a6, reversing
    changes made to dfe50831637c9ada2593010a7197867e1ccdd168.

--- Assets/Scripts/Components/EmployeeManagerUIScript.cs
+++ Assets/Scripts/Components/EmployeeManagerUIScript.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EmployeeManagerUIScript : MonoBehaviour {
-    [SerializeField] GameObject employeeUIPrefab;
-    [SerializeField] Transform employeeUIContent;
-
-    public void AddEmployee(Employee employee) { 
-        GameObject employeeUI = Instantiate(employeeUIPrefab, employeeUIContent);
-        employeeUI.GetComponent<EmployeeUIScript>().SetData(employee);
+    void Start() {
+        
     }
 
+    void Update() {
+        
+    }
 }
--- Assets/Scripts/Components/Singletons/EmployeeManager.cs
+++ Assets/Scripts/Components/Singletons/EmployeeManager.cs
@@ -6,11 +6,8 @@ using UnityEngine;
 public class EmployeeManager : Singleton<EmployeeManager> {
     [HideInInspector] public List<Employee> Employees = new();
 
-    [SerializeField] EmployeeManagerUIScript ui;
-
     public void AddEmployee(Employee employee) {
         Employees.Add(employee);
-        ui.AddEmployee(employee);
     }
 
     public void RemoveEmployee(Employee employee) {
