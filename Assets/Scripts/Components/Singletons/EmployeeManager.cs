7b873012d73e6b772adab37b4a5284ff831a5ba6
MaxWesterlund <max.westerlund3@gmail.com>
2023-07-25 18:54:34 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/lomstfer/Applicationeer""
    This reverts commit 9fcfeaa0869263cb7f293bb6e4c861382f46aa28.

--- Assets/Scripts/Components/Singletons/EmployeeManager.cs
+++ Assets/Scripts/Components/Singletons/EmployeeManager.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class EmployeeManager : Singleton<EmployeeManager> {
     [HideInInspector] public List<Employee> Employees = new();
 
+    [SerializeField] EmployeeManagerUIScript ui;
+
     public void AddEmployee(Employee employee) {
         Employees.Add(employee);
+        ui.AddEmployee(employee);
     }
 
     public void RemoveEmployee(Employee employee) {
