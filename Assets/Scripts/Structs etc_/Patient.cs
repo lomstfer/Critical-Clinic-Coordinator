26319a93912abb33f19e01d70e7a278b50db4730
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-07-30 23:00:03 +0200

    .

--- Assets/Scripts/Structs etc_/Patient.cs
+++ Assets/Scripts/Structs etc_/Patient.cs
@@ -8,14 +8,9 @@ public class Patient {
     public string LastName;
 
     public Skill[] Syndromes;
-
-    // The amount of ResponsibleEmployees that are necessary to heal;
-    //public int SyndromeExtremeness;
     
     public List<Employee> ResponsibleEmployees = new();
 
-    // 100 is healthy, 0 is dead.
-    // Ticking down by the minute by 1 and up by 2 if ResponsibleEmployees.Count is >= SyndromeExtremeness;
     public int Healthyness;
 
     public int SedateTime;
